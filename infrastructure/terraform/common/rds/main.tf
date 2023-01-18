ed1793353917dda1b849b33d96d982f9ecea72ed
Pehesi97 <pedro.silva@wisenet.inf.br>
2023-01-18 15:43:00 -0300

    fix: adding useful db info to the secret in secretsManager

--- infrastructure/terraform/common/rds/main.tf
+++ infrastructure/terraform/common/rds/main.tf
@@ -1,15 +1,31 @@
+locals {
+  db_engine = "postgres"
+  db_name = "tododb"
+  db_username = "otlpuser"
+}
+
 resource "random_password" "db_password" {
   length           = 20
   special          = false
 }
 
 resource "aws_secretsmanager_secret" "database_secrets_manager" {
-  name = "${var.deployment_env}/rds/postgres_db"
+  name = "${var.deployment_app_name}-${var.deployment_env}/rds/postgres_db"
 }
 
 resource "aws_secretsmanager_secret_version" "secret_version" {
   secret_id     = aws_secretsmanager_secret.database_secrets_manager.id
-  secret_string = random_password.db_password.result
+  secret_string = <<EOF
+  {
+    "database": "${aws_db_instance.database.db_name}",
+    "username": "${local.db_username}",
+    "password": "${random_password.db_password.result}",
+    "engine": "${local.db_engine}",
+    "host": "${aws_db_instance.database.endpoint}",
+    "port": ${aws_db_instance.database.port},
+    "identifier": "${aws_db_instance.database.identifier}"
+  }
+  EOF
 }
 
 
@@ -19,38 +35,16 @@ resource "aws_db_subnet_group" "subnet_group" {
 }
 
 resource "aws_db_instance" "database" {
-  allocated_storage      = 30
-  engine                 = "postgres"
+  identifier             = "${var.deployment_app_name}-${var.deployment_env}-postgres-db" 
+  allocated_storage      = 20
+  engine                 = local.db_engine
   engine_version         = "14.2"
   instance_class         = "db.t3.medium"
-  db_name                = "tododb"
-  username               = "otlpuser"
-  password               = aws_secretsmanager_secret_version.secret_version.secret_string
+  db_name                = local.db_name
+  username               = local.db_username
+  password               = random_password.db_password.result
   db_subnet_group_name   = aws_db_subnet_group.subnet_group.name
   skip_final_snapshot    = true
   multi_az               = true
   vpc_security_group_ids = [var.rds_sg_id]
-}
-
-resource "null_resource" "database_setup" {
-  triggers = {
-    setup_script_sha1 = filesha1("${var.deployment_parent_dir}/../../postgresql/docker_postgres_init.sql")
-  }
-
-  provisioner "local-exec" {
-    command = <<-EOF
-			while read line; do
-				echo "$line"
-				aws rds-data execute-statement --resource-arn "$DB_ARN" --database  "$DB_NAME" --secret-arn "$SECRET_ARN" --sql "$line"
-			done  < <(awk 'BEGIN{RS=";\n"}{gsub(/\n/,""); if(NF>0) {print $0";"}}' ${var.deployment_parent_dir}/../../postgresql/docker_postgres_init.sql)
-			EOF
-    environment = {
-      DB_ARN     = aws_db_instance.database.arn
-      DB_NAME    = aws_db_instance.database.db_name
-      SECRET_ARN = aws_secretsmanager_secret.database_secrets_manager.arn
-    }
-    interpreter = ["bash", "-c"]
-  }
-
-  depends_on = [aws_db_instance.database]
 }
