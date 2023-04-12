a27110277be0840ebc2644acbe72e70d3e9cbeac
brainrepo <murru7@gmail.com>
2023-04-12 09:25:41 +0200

    enrich metrics with attributes

--- pkg-svcs/backend/lib/plugins/metrics/index.js
+++ pkg-svcs/backend/lib/plugins/metrics/index.js
@@ -1,19 +1,19 @@
 const fp = require('fastify-plugin')
-
 async function plugin(server, options) {
-    server.addHook('onRequest', (req, res, done) => {
+    const hostname = require('os').hostname()
+
+    server.addHook('onRequest', (req, _res, done) => {
+        console.log(options)
+        const [{ port }] = server.addresses()
         const { method, protocol: scheme, routerPath } = req
-        server.monitoring.requestCounter.add(1, { path: routerPath })
-        server.monitoring.activeRequests.add(1, { method, scheme })
-        //Timeout helps to keep this request
-        // setTimeout(() => {
-        //     done()
-        // }, 10000)
+        server.monitoring.requestCounter.add(1, { path: routerPath, method, scheme, 'net.host.name': hostname, 'net.host.port': port })
+        server.monitoring.activeRequests.add(1, { method, scheme, 'net.host.name': hostname, 'net.host.port': port })
         done()
     })
     server.addHook('onResponse', (req, _res, done) => {
         const { method, protocol: scheme } = req
-        server.monitoring.activeRequests.add(-1, { method, scheme })
+        const [{ port }] = server.addresses()
+        server.monitoring.activeRequests.add(-1, { method, scheme, 'net.host.name': hostname, 'net.host.port': port })
         done()
     })
 }
