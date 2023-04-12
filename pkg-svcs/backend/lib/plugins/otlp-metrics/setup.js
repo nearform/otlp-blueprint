96e60254fc6016e9542a6a04b8a215cf254a305b
brainrepo <murru7@gmail.com>
2023-04-12 11:05:56 +0200

    fix: lint

--- pkg-svcs/backend/lib/plugins/otlp-metrics/setup.js
+++ pkg-svcs/backend/lib/plugins/otlp-metrics/setup.js
@@ -1,49 +1,60 @@
-const { DiagConsoleLogger, DiagLogLevel, diag } = require('@opentelemetry/api');
-const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http');
-const { MeterProvider, PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
-const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { DiagConsoleLogger, DiagLogLevel, diag } = require('@opentelemetry/api')
+const {
+  OTLPMetricExporter
+} = require('@opentelemetry/exporter-metrics-otlp-http')
+const {
+  MeterProvider,
+  PeriodicExportingMetricReader
+} = require('@opentelemetry/sdk-metrics')
+const { Resource } = require('@opentelemetry/resources')
+const {
+  SemanticResourceAttributes
+} = require('@opentelemetry/semantic-conventions')
 
 const enableMonitoring = options => {
-    diag.setLogger(
-        new DiagConsoleLogger(),
-        options.debug ? DiagLogLevel.DEBUG : DiagLogLevel.INFO
-    )
+  diag.setLogger(
+    new DiagConsoleLogger(),
+    options.debug ? DiagLogLevel.DEBUG : DiagLogLevel.INFO
+  )
 
-    const metricExporter = new OTLPMetricExporter({
-        url: `${options.collectorUrl}/v1/metrics`,
-        serviceName: options.serviceName,
-        concurrencyLimit: 10
-    });
+  const metricExporter = new OTLPMetricExporter({
+    url: `${options.collectorUrl}/v1/metrics`,
+    serviceName: options.serviceName,
+    concurrencyLimit: 10
+  })
 
-    const meterProvider = new MeterProvider({
-        resource: new Resource({
-            [SemanticResourceAttributes.SERVICE_NAME]: 'basic-metric-service',
-        }),
-    });
-
-    meterProvider.addMetricReader(new PeriodicExportingMetricReader({
-        exporter: metricExporter,
-        exportIntervalMillis: 1000,
-    }));
-
-    // Meter definitions
-    const meter = meterProvider.getMeter('metrics-collector');
-
-    const requestCounter = meter.createCounter('http.server.requests', {
-        description: 'Requests counter',
-    });
-
-    const activeRequests = meter.createUpDownCounter('http.server.active_requests', {
-        description: 'Monitor the active requests in the server'
+  const meterProvider = new MeterProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: 'basic-metric-service'
     })
+  })
 
-    const requestTimes = meter.createHistogram('http.server.request.times', {
-        description: 'Histogram showing the request times'
+  meterProvider.addMetricReader(
+    new PeriodicExportingMetricReader({
+      exporter: metricExporter,
+      exportIntervalMillis: 1000
     })
+  )
 
+  // Meter definitions
+  const meter = meterProvider.getMeter('metrics-collector')
 
-    return { requestCounter, activeRequests, requestTimes }
+  const requestCounter = meter.createCounter('http.server.requests', {
+    description: 'Requests counter'
+  })
+
+  const activeRequests = meter.createUpDownCounter(
+    'http.server.active_requests',
+    {
+      description: 'Monitor the active requests in the server'
+    }
+  )
+
+  const requestTimes = meter.createHistogram('http.server.request.times', {
+    description: 'Histogram showing the request times'
+  })
+
+  return { requestCounter, activeRequests, requestTimes }
 }
 
 module.exports = { enableMonitoring }
