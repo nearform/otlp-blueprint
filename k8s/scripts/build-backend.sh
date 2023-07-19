9ceae5daa32086b2f66dc3c7ec7b4dbd91a72061
Heider Leandro Massu Hengstmann <heidermassu@gmail.com>
2023-07-18 21:17:03 -0300

    lb-collector

--- k8s/scripts/build-backend.sh
+++ k8s/scripts/build-backend.sh
@@ -1,7 +1,6 @@
 set -e
 
-image_suffix="${1:-"otlp-blueprint"}"
+image_suffix=${1:-"otlp-blueprint"}
 
-pushd ../
+cd ../
   docker build -t "$image_suffix"-backend -f Backend.dockerfile .
-popd
--- k8s/scripts/otel-operator.sh
+++ k8s/scripts/otel-operator.sh
@@ -2,8 +2,8 @@ set -e
 
 # Remove the sleeps checking that the resources are ready before proceeding
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.0/cert-manager.yaml
-sleep 30
+sleep 50
 kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
-sleep 30
+sleep 50
 
 
