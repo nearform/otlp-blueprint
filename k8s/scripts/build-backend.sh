261b327549678ec36dc69d3c7e3bcffc3ac3acce
heidermassu <heider.massu@nearform.com>
2023-07-20 13:33:15 -0300

    fixing issues related test demo

--- k8s/scripts/build-backend.sh
+++ k8s/scripts/build-backend.sh
@@ -1,7 +1,7 @@
 set -e
 
-image_suffix="${1:-"otlp-blueprint"}"
+image_suffix=${1:-$IMAGE_NAME}
 
-pushd ../
-  docker build -t "$image_suffix"-backend -f Backend.dockerfile .
-popd
+cd ../
+  docker build -t $image_suffix-backend -f Backend.dockerfile .
+cd -
